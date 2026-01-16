01c2ee01288a2f9f465daba07d0c4d2d061c4f95
Grim <makc.maka@gmail.com>
2026-01-16 11:57:22 +0100

    HW7 all done

--- homeworks/homework_07/HW7.py
+++ homeworks/homework_07/HW7.py
@@ -71,14 +71,25 @@ print(reverse_string('Hello, world!'))
 """
 print(f'\nTask 5')
 
+def longest_word(list_of_words: list) -> str:
+    return max(list_of_words, key=len, default=None)
 
+some_list = ['Мені', 'тринадцятий', 'минало', 'я', 'пас', 'ягнята', 'за', 'селом']
+print(longest_word(some_list))
 
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
 у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
 не є підрядком першого рядка."""
-def find_substring(str1, str2):
+print(f'\nTask 6')
 
+def find_substring(str1, str2):
+    if str2 == "":
+        return 0
+
+    for i in range(len(str1) - len(str2) + 1):
+        if str1[i:i + len(str2)] == str2:
+            return i
     return -1
 
 str1 = "Hello, world!"
@@ -90,9 +101,65 @@ str2 = "cat"
 print(find_substring(str1, str2)) # поверне -1
 
 # task 7
+# домашка 1 таска 8
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+print(f'\nTask 7')
+
+# first_part_of_the_day = 5
+# second_part_of_the_day = first_part_of_the_day - 10
+# last_part_of_the_day = second_part_of_the_day + 4
+# print(last_part_of_the_day)
+
+def final_temperature(first_part_of_the_day: int, second_part_of_the_day: int, third_part_of_the_day: int) -> int:
+    temperature = first_part_of_the_day + second_part_of_the_day + third_part_of_the_day
+    return temperature
+
+print(final_temperature(5, -10, 4))
+
 # task 8
+# домашка 1 таска 6 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+print(f'\nTask 8')
+# perimetery = storona_1 + storona_2 + storona_3 + storona_4
+# print(perimetery)
+
+def perimeter(first_side: int, second_side: int, third_side: int, fourth_side: int) -> int:
+    return first_side + second_side + third_side + fourth_side
+
+print(perimeter(5, 4, 4, 5))
+
 # task 9
+# Буде рекорд_свапер як в домашці 5_2, таска друга.
+print(f'\nTask 9')
+
+list_task9 = ['Мені', 'тринадцятий', 'минало', 'я', 'пас', 'ягнята', 'за', 'селом']
+
+def recordswaper (some_list: list, first_index: int, second_index: int):
+    some_list[first_index], some_list[second_index] = some_list[second_index], some_list[first_index]
+    print(some_list)
+
+recordswaper(list_task9, 2, 6)
+
 # task 10
+# З домашки 6_1
+# Порахувати кількість унікальних символів в строці.
+# Якщо їх більше 10 - вивести в консоль True, інакше - False.
+# Строку отримати за допомогою функції input()
+
+# user_text = input('Type ur text pls: ')
+# print(len(set(user_text)) > 10)
+print(f'\nTask 10')
+
+def has_more_than_10_unique_chars(text: str) -> bool:
+    return len(set(text)) > 10
+
+print(has_more_than_10_unique_chars('тринадцятий'))
+print(has_more_than_10_unique_chars('тринадцятий минало'))
+
 """  Оберіть будь-які 4 таски з попередніх домашніх робіт та
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
 Обоязково документуйте функції та дайте зрозумілі імена змінним.
